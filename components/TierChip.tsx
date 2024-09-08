dc87c9951b86a20cdc107f3b438cb55d96225ebe
lim0n3 <simonestaffa96@gmail.com>
2024-09-08 03:56:10 +0200

    Chip radius

--- components/TierChip.tsx
+++ components/TierChip.tsx
@@ -21,6 +21,7 @@ export default function TierChip({ tier }: { tier: UserTier }) {
     return (
       <Chip
         variant="shadow"
+        radius="sm"
         classNames={{
           base: "bg-gradient-to-br from-gray-200 to-gray-300 border-small border-white/50 shadow-gray-300/30",
           content: "drop-shadow shadow-black text-gray-700",
@@ -34,6 +35,7 @@ export default function TierChip({ tier }: { tier: UserTier }) {
     return (
       <Chip
         variant="shadow"
+        radius="sm"
         classNames={{
           base: "bg-gradient-to-br from-amber-200 to-yellow-300 border-small border-white/50 shadow-yellow-300/30",
           content: "drop-shadow shadow-black text-amber-800",
